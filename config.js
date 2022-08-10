f11d6e9a14333b37bc194e2562a8223d6d69db33
Olavitsme1 <109358970+Olavitsme1@users.noreply.github.com>
2022-08-10 22:21:40 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6281319339702'],
+  ['6281319339702', 'olvv', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = 'olvv'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
